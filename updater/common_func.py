d495f45002896d9fcd59c92ae181afdca6791d2d
Doney den Ouden <doney@doney.nl>
2026-02-27 14:43:48 +0100

    Move from ja to ja_nk

--- updater/common_func.py
+++ updater/common_func.py
@@ -5,8 +5,8 @@ import shutil
 edit below to add/remove languages
 """
 # must mach lang code in LangCodeSelectableList.java
-LANG = ['pt_br', 'no', 'es_ag', 'ja', 'ja_beginner', 'ru', 'tr'] # add language codes here to add new language, then use update_nonEn_transcripts.py (and update_char_images.py if its non alphabet characters)
-LANG_CODE_STANDARD = ['pt-BR', 'no', 'es-AR', 'ja', 'ja-beginner', 'ru', 'tr'] #  language codes follow the ISO 639 standard
+LANG = ['pt_br', 'no', 'es_ag', 'ja', 'ja_nk', 'ru', 'tr'] # add language codes here to add new language, then use update_nonEn_transcripts.py (and update_char_images.py if its non alphabet characters)
+LANG_CODE_STANDARD = ['pt-BR', 'no', 'es-AR', 'ja', 'ja-nk', 'ru', 'tr'] #  language codes follow the ISO 639 standard
 # files not to list on hash file
 EXTENSION_IGNORE = ['.xlsx', 'xliff']
 
